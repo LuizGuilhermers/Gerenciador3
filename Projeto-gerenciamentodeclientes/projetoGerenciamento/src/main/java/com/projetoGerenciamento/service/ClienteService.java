8dccbb08fcffa745f352ac9d927f2e14a0d3d179
LuizGuilhermers <132360055+LuizGuilhermers@users.noreply.github.com>
2023-10-24 10:49:13 -0300

    Add files via upload

---
+++ Projeto-gerenciamentodeclientes/projetoGerenciamento/src/main/java/com/projetoGerenciamento/service/ClienteService.java
@@ -0,0 +1,57 @@
+package com.projetoGerenciamento.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetoGerenciamento.entities.Cliente;
+import com.projetoGerenciamento.repository.ClienteRepository;
+
+@Service
+public class ClienteService {
+	
+	private final ClienteRepository clienteRepository;
+
+	@Autowired
+	public ClienteService (ClienteRepository clienteRepository) {
+		this.clienteRepository = clienteRepository;
+	}
+
+	public List<Cliente> buscaTodosClientes () {
+		return clienteRepository.findAll ();
+	}
+	
+	public Cliente buscaClienteld (Long id) {
+	Optional <Cliente> Cliente = clienteRepository.findById(id); 
+	return Cliente.orElse(null);
+	}
+
+	public Cliente salvaCliente (Cliente cliente) 
+	{
+		return clienteRepository.save(cliente);
+	}
+	
+	public Cliente alterarCliente (Long id, Cliente alterarC) {
+		Optional <Cliente> existeCliente = clienteRepository.findById(id);
+		if(existeCliente.isPresent()) {
+			alterarC.setId(id);
+			return clienteRepository.save(alterarC);
+		}
+		
+		return null;
+	}
+		
+		public boolean apagarCliente (Long id) {
+			Optional <Cliente> existeCliente = clienteRepository.findById(id);
+			if (existeCliente.isPresent()) {
+				clienteRepository.deleteById(id);
+				return true;
+			}
+			return false;
+		
+	}
+	
+
+}
